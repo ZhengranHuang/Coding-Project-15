8302dbab9c77a294e9f194f79bbb162d6e9df563
ZhengranHuang <Zhengran@usf.edu>
2024-11-09 13:02:14 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions.

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+//Task2: Create the Portfolio Module
+import {assetList} from './asset.js';
+
+export function calculatePortfolioValue() {
+return assetList.reduce((total, asset) => total + (asset.price * asset.quantity), 0);
+};
+const totalPortfolioValue = calculatePortfolioValue();
+export function getPortfolioAllocation(){
+return asset.map(asset => (asset.price*asset.quantity)/totalPortfolioValue)*100;
+}
+calculatePortfolioValue(1)
